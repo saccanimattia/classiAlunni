f98ba39051d42a1e0f605e25001acefa5e676e66
saccanimattia <mattia.saccani@studenti.iispascal.it>
2023-05-31 14:56:41 +0200

    routing

---
+++ src/app/servizi/servizioProva/servizio-prova.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServizioProvaService {
+
+  persone = [
+    {nome:"luca", cognome:"rosi", isOnline:true, colore:'blue'},
+    {nome:"marco", cognome:"verdi", isOnline:true, colore:'yellow'},
+    {nome:"daniele", cognome:"neri", isOnline:false, colore:'orange'},
+    {nome:"luca", cognome:"rosi", isOnline:false, colore:'blue'},
+    {nome:"marco", cognome:"verdi", isOnline:false, colore:'green'},
+    {nome:"daniele", cognome:"neri", isOnline:true, colore:'blue'}
+  ];
+
+  constructor() { }
+}
