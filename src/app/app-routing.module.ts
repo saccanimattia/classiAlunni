cc1ab61793f6d4db55d12eac23efef5d70ed1649
saccanimattia <mattia.saccani@studenti.iispascal.it>
2023-06-01 10:39:11 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './componenti/home/home.component';
-import { AboutComponent } from './componenti/about/about.component';
-import { ContactComponent } from './componenti/contact/contact.component';
+import { ContattiComponent} from './componenti/contatti/contatti.component';
+import { ContattoComponent} from './componenti/contatto/contatto.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent},
+  {path: 'contatti', component: ContattiComponent, children: [
+    {path: ':id', component: ContattoComponent}
+  ]},
 ];
 
 @NgModule({
